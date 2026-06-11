247c95fb0a150fa62b12c1305fff2f5d30e1522a
Manish <manish@gmail.com>
2026-06-11 12:53:16 +0530

    Implemented user registration API

---
+++ Portfolio-tracker/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio_tracker.Data;
+using Portfolio_tracker.DTOs;
+using Portfolio_tracker.Models;
+
+namespace Portfolio_tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequestDto request)
+        {
+            var user = new User
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                PasswordHash = request.Password,
+                CreateAt = DateTime.Now
+            };
+
+            _context.User.Add(user);
+            _context.SaveChanges();
+
+            return Ok(new
+            {
+                Message = "Registration request is working",
+                Data = request
+            });
+        }
+    }
+}
