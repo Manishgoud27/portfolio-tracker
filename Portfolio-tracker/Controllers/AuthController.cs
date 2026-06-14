0dea74f3b89eb562f9641724fe544e450fe08307
Manish <manish@gmail.com>
2026-06-14 13:04:28 +0530

    Added Jwt Authentication and Authorization

--- Portfolio-tracker/Controllers/AuthController.cs
+++ Portfolio-tracker/Controllers/AuthController.cs
@@ -3,6 +3,12 @@ using Microsoft.EntityFrameworkCore;
 using Portfolio_tracker.Data;
 using Portfolio_tracker.DTOs;
 using Portfolio_tracker.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
 
 
 namespace Portfolio_tracker.Controllers
@@ -12,9 +18,12 @@ namespace Portfolio_tracker.Controllers
     public class AuthController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        public AuthController(ApplicationDbContext context)
+
+        private readonly IConfiguration _configuration;
+        public AuthController(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
@@ -60,7 +69,47 @@ namespace Portfolio_tracker.Controllers
                 return Unauthorized();
             }
 
-            return Ok("Login successfull");
+            var token = GenerateJwtToken(user);
+
+            return Ok(new
+            {
+                Token = token
+            });
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var key = _configuration["jwt:key"];
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!));
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var credentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["jwt:Issuer"],
+                audience: _configuration["jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials
+                );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            return tokenHandler.
+                WriteToken(token);
+        }
+
+        [Authorize]
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("Authenticated");
         }
     }
 }
--- Portfolio-tracker/Program.cs
+++ Portfolio-tracker/Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Portfolio_tracker.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +16,58 @@ options.UseSqlServer(
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Enter JWT Token"
+    });
+
+    options.AddSecurityRequirement(
+        new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+        {
+            {
+                new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                    {
+                        Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+});
+
+var jwtsettings = builder.Configuration.GetSection("jwt");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtsettings["issuer"],
+            ValidAudience = jwtsettings["audience"],
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtsettings["key"]!))
+        };
+    });
 
 var app = builder.Build();
 
@@ -25,6 +79,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
